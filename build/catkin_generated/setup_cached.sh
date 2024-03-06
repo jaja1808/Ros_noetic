e080baf3c00bb6712f2c46f66d42c6a4da307e1a
Prince <rushayigiprince2502@gmail.com>
2024-03-06 17:30:31 +0100

    follower camera rqt and rviz

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export PKG_CONFIG_PATH="/home/esirem/Documents/Project_ws/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/esirem/Documents/Project_ws/catkin_ws/build'
