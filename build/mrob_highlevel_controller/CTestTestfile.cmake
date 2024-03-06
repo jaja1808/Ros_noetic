9d78f30c7d925f48615d5531c73e5c9ca1a04843
Prince <rushayigiprince2502@gmail.com>
2024-03-06 13:43:16 +0100

    aruco start

--- build/mrob_highlevel_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/esirem/Documents/Project_ws/catkin_ws/src/mrob_highlevel_controller
-# Build directory: /home/esirem/Documents/Project_ws/catkin_ws/build/mrob_highlevel_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
